0843b22ea285cacf607c3d2da46c3f4c7a0f9593
donabinu921 <elizabethdona921@gmail.com>
2023-05-19 10:33:32 +0530

    removeTransition function defined;EventListener added; js file set up complete

--- main.js
+++ main.js
@@ -6,3 +6,11 @@ function playSound(e){
     audio.play();
 }
 
+function removeTransition(e){
+    if(e.propertyName!=='transform') return;
+    e.target.classList.remove('playing');
+}
+
+window.addEventListener('keydown', playSound);
+const keys=Array.from(document.querySelectorAll('.keys'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
